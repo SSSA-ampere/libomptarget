4a3630094e944a65e62aa8d905f06d9c94fd8f27
Hervé Yviquel <hyviquel@gmail.com>
2015-09-29 18:03:55 -0300

    Improve scala library so as to minimize the scala code generation

---
+++ DevRTLs/spark/src/main/scala/org/llvm/openmp/CloudInfo.scala
@@ -0,0 +1,39 @@
+package org.llvm.openmp
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkFiles
+import org.apache.spark.rdd.RDD
+
+class CloudInfo(var uri: String, var username: String, var path: String) {
+
+  val conf = new SparkConf() //.setAppName("HdfsTest").setMaster("local")
+  val sc = new SparkContext(conf)
+  
+  // Load library containing native kernel
+  sc.addFile(fullpath + "libmr.so")
+  System.load(SparkFiles.get("libmr.so"))
+
+  def write(name: Integer, data: Array[Byte]): Unit = {
+    System.setProperty("HADOOP_USER_NAME", username)
+    val filepath = new Path(path + name)
+    val conf = new Configuration()
+    conf.set("fs.defaultFS", uri)
+    val fs = FileSystem.get(conf)
+    val os = fs.create(filepath)
+    os.write(data)
+    fs.close()
+  }
+
+  def read(id: Integer, size: Integer): RDD[Array[Byte]] = {
+    sc.binaryRecords(uri + path + id, 4)
+  }
+
+  def fullpath = {
+    uri + path
+  }
+
+}
--- DevRTLs/spark/src/main/scala/org/llvm/openmp/Util.scala
+++ DevRTLs/spark/src/main/scala/org/llvm/openmp/Util.scala
@@ -1,25 +1,24 @@
 package org.llvm.openmp
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkFiles
 import org.apache.spark.rdd.RDD
 
 object Util {
-  
-  def loadLibrary(sc: SparkContext, info: HdfsInfo) {
-    sc.addFile(info.fullpath + "libmr.so")
-    System.load(SparkFiles.get("libmr.so"))
-  }
 
   // zip the RDDs into an RDD of Seq[Int]
   def makeZip[U](s: Seq[RDD[U]]): RDD[Seq[U]] = {
-    if (s.length == 1) 
-      s.head.map(e => Seq(e)) 
+    if (s.length == 1)
+      s.head.map(e => Seq(e))
     else {
       val others = makeZip(s.tail)
       val all = s.head.zip(others)
       all.map(elem => Seq(elem._1) ++ elem._2)
     }
   }
-  
+
 }
