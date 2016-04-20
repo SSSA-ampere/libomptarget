258f3d810c48807bbbcffdb1a9260a98b62fdfd7
Hervé Yviquel <hyviquel@gmail.com>
2016-04-20 15:26:00 -0300

    add bitor of bytearray

--- DevRTLs/spark/src/main/scala/org/llvm/openmp/Util.scala
+++ DevRTLs/spark/src/main/scala/org/llvm/openmp/Util.scala
@@ -7,6 +7,8 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkFiles
 import org.apache.spark.rdd.RDD
+import java.nio.ByteBuffer
+import java.nio.IntBuffer
 
 object Util {
 
@@ -19,6 +21,26 @@ object Util {
       val all = s.head.zip(others)
       all.map(elem => Seq(elem._1) ++ elem._2)
     }
+  }
+  
+  def bitor(x: Array[Byte], y: Array[Byte]) : Array[Byte] = {
+    val bx = ByteBuffer.wrap(x).array()
+    val by = ByteBuffer.wrap(y).array()
+    for(i <- 0 to bx.size-1) {
+      bx(i) = (bx(i) | by(i)).toByte
+    }
+    return bx
+  }
+  
+  def bitor2(x: Array[Byte], y: Array[Byte]) : Array[Byte] = {
+    (x.toStream zip y.toStream).map{ case (a,b) => (a|b).toByte }.toArray
+  }
+  
+  def bitor3(x: Array[Byte], y: Array[Byte]) : Array[Byte] = {
+    val bx = ByteBuffer.wrap(x).asIntBuffer().array()
+    val by = ByteBuffer.wrap(y).asIntBuffer().array()
+    val hi = (bx.toStream zip by.toStream).map{ case (a,b) => (a|b) }.toArray
+    return x
   }
 
 }
