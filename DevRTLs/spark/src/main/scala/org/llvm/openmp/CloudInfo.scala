22ea8fb80c447c102680da0f74b40ecb3ba02861
Hervé Yviquel <hyviquel@gmail.com>
2017-01-25 17:26:50 -0200

    Make DevRTL lib compilable for spark 1.6.x

--- DevRTLs/spark/src/main/scala/org/llvm/openmp/CloudInfo.scala
+++ DevRTLs/spark/src/main/scala/org/llvm/openmp/CloudInfo.scala
@@ -7,10 +7,10 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkFiles
 import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.SparkSession
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.fs.FileUtil
 import java.io.File
+import org.apache.hadoop.conf.Configuration
 
 object NativeKernels {
 
@@ -32,53 +32,56 @@ class CloudInfo(args: Array[String]) {
   val uri = args(1)
   val username = args(2)
   val path = args(3)
-  
+
   val conf = new SparkConf()
     //.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     //.registerKryoClasses(Array(classOf[CloudFileSystem], classOf[CloudInfo]))
     //.set("spark.kryoserializer.buffer.max", "2047m")
     .set("spark.driver.maxResultSize", "0")
-  
-  val session = SparkSession.builder().config(conf).getOrCreate()
-  
-  val sc = session.sparkContext
-  
+
+  //val session = SparkSession.builder().config(conf).getOrCreate()
+
+  //val sc = session.sparkContext
+  val sc = new SparkContext(conf)
+
+  var fsConf = new Configuration
+
   filesystem match {
     case "S3" =>
-      sc.hadoopConfiguration.set("fs.s3a.impl","org.apache.hadoop.fs.s3a.S3AFileSystem")
-      sc.hadoopConfiguration.set("fs.s3a.awsAccessKeyId", args(4))
-      sc.hadoopConfiguration.set("fs.s3a.awsSecretAccessKey", args(5))
-      
-      //sc.hadoopConfiguration.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
-      //sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", accessKey)
-      //sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", secretKey)
-      
+      //fsConf.set("fs.s3a.impl","org.apache.hadoop.fs.s3a.S3AFileSystem")
+      //fsConf.set("fs.s3a.awsAccessKeyId", args(4))
+      //fsConf.set("fs.s3a.awsSecretAccessKey", args(5))
+
+      fsConf.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+      fsConf.set("fs.s3n.awsAccessKeyId", args(4))
+      fsConf.set("fs.s3n.awsSecretAccessKey", args(5))
+
       //sc.hadoopConfiguration.set("fs.defaultFS", uri)
-    case "HDFS" => 
+    case "HDFS" =>
       System.setProperty("HADOOP_USER_NAME", username)
-      //sc.hadoopConfiguration.set("fs.defaultFS", uri)
+      fsConf = sc.hadoopConfiguration
     case _ => throw new RuntimeException(filesystem + " is not a supported file system.")
   }
-  
+
   val sqlContext = new SQLContext(sc)
-  
-  val fs = FileSystem.get(URI.create(uri), sc.hadoopConfiguration)
-  
+
+  val fs = FileSystem.get(URI.create(uri), fsConf)
+
   val localLibrary = new File("/tmp/", NativeKernels.LibraryName)
-  
-  FileUtil.copy(fs, new Path(uri + path + NativeKernels.LibraryName), localLibrary, false, sc.hadoopConfiguration)
+
+  FileUtil.copy(fs, new Path(uri + path + NativeKernels.LibraryName), localLibrary, false, fsConf)
 
   def init(fs: CloudFileSystem) {
     // Load library containing native kernel
     sc.addFile(localLibrary.getAbsolutePath)
   }
-  
+
   def getExecutorNumber(): Integer = {
     var nb = sc.getExecutorStorageStatus.length
     if(nb > 1) nb = nb - 1 // Do not count the driver node
     return nb
   }
-  
+
   def getParallelism(): Integer = {
     val parallelism = sc.getConf.getInt("spark.default.parallelism", sc.defaultParallelism)
     return parallelism
