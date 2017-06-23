2ad3b9007760c505c52088d627d1aeb398f32970
Hervé Yviquel <hyviquel@gmail.com>
2017-06-23 16:25:23 -0300

    rename file instead of copying them on local version

--- RTLs/cloud/src/providers/local.cpp
+++ RTLs/cloud/src/providers/local.cpp
@@ -64,17 +64,19 @@ std::string LocalProvider::get_cloud_path(std::string filename) {
 
 int32_t LocalProvider::send_file(std::string filename,
                                  std::string tgtfilename) {
-  std::ifstream src(filename, std::ios::binary);
-  std::ofstream dst(get_cloud_path(tgtfilename), std::ios::binary);
-  dst << src.rdbuf();
+  remove(get_cloud_path(tgtfilename).c_str());
+  int rc = rename(filename.c_str(), get_cloud_path(tgtfilename).c_str());
+  if (rc < 0)
+    perror("Error renaming file");
   return OFFLOAD_SUCCESS;
 }
 
 int32_t LocalProvider::get_file(std::string host_filename,
                                 std::string filename) {
-  std::ifstream src(get_cloud_path(filename), std::ios::binary);
-  std::ofstream dst(host_filename, std::ios::binary);
-  dst << src.rdbuf();
+  remove(host_filename.c_str());
+  int rc = rename(get_cloud_path(filename).c_str(), host_filename.c_str());
+  if (rc < 0)
+    perror("Error renaming file");
   return OFFLOAD_SUCCESS;
 }
 
